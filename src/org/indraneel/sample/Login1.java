f696e9566503904cc0c39dbbd8b1ca6d7cfcef53
arampall <arampall@uncc.edu>
2015-10-27 17:54:14 -0400

    login and signup

--- src/org/indraneel/sample/Login1.java
+++ src/org/indraneel/sample/Login1.java
@@ -1,6 +1,7 @@
 package org.indraneel.sample;
-
+import java.sql.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,34 +9,95 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class Login1
+ * Servlet implementation class SimpleServlet
  */
-@WebServlet("/Login1")
+@WebServlet(description = "A simple servlet example", urlPatterns = { "/Login1" })
 public class Login1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Login1() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
+	
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		System.out.println("\n Coming Here");
+		try {
+		     Class.forName("com.mysql.jdbc.Driver");
+		     System.out.println("Driver found");
+				
+				
+				String url="jdbc:mysql://localhost/mydb";
+				String user="indra";
+				String password="tp123";
+				Connection con=null;
+				
+				
+					con=DriverManager.getConnection(url, user, password);
+					System.out.println("\n Connected successfully");
+					Statement stmt = con.createStatement();
+					String sql = "SELECT * FROM login";
+					ResultSet rs = stmt.executeQuery(sql);
+				
+				
+				
+				
+		PrintWriter out=response.getWriter();
+		response.setContentType("text/html");// TODO Auto-generated method stub
+		String username=request.getParameter("username");
+		String password1=request.getParameter("password");
+		int flag=0;
+		
+		 
+				
+				while(rs.next()){
+			         //Retrieve by column name
+			         
+			         String use = rs.getString("username");
+			         String pass = rs.getString("password");
+			         System.out.println("\n"+use);
+			         System.out.println("\n"+pass);
+			         if(use.equals(username)&&pass.equals(password1))
+			         {
+			        	 flag=1;
+			        	 break;
+			         }
+				}
+			
+			 
+		
+		if(flag==0)
+		{
+			out.println("<html>");
+            out.println("<title>Invalid login or password</title>");
+            out.println("<body>");
+            out.println("<H1>Invalid login or password</H1>");
+            out.println("<FORM action=Login1>");
+            out.println("USERNAME :");
+            out.println("<INPUT type= text name= username>");
+            out.println("<br>");
+            out.println("PASSWORD: <input type=password name=password>");
+            out.println("<br>");
+            out.println("<INPUT type=submit value=login>");
+            out.println("</FORM>");
+            out.println("<H2 align=left>Dont have an account,create one</H2><FORM action=signup.html><input type=submit value=Create new account></FORM>");
+            out.println("</body>");
+            out.println("</html>");
+		}
+		else
+		{
+			out.println("<html><body><h1> Welcome back</h1></body></html>");
+		}
+		
+		}catch(SQLException e)
+		{
+			System.out.println("\n Something went wrong in the connection string");
+		}
+		
+		catch(ClassNotFoundException e)
+		{
+			System.out.println("Driver not found");
+		}
+		
 	}
 
 }
--- src/org/indraneel/sample/SignUp.java
+++ src/org/indraneel/sample/SignUp.java
@@ -1,6 +1,16 @@
 package org.indraneel.sample;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,32 +20,101 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class SignUp
  */
-@WebServlet("/SignUp")
+@WebServlet(description = "Signing up", urlPatterns = { "/SignUp" })
 public class SignUp extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SignUp() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		System.out.println("Inside Signup");
+		try {
+		     Class.forName("com.mysql.jdbc.Driver");
+		     System.out.println("Driver found");
+				
+				
+				String url="jdbc:mysql://localhost/tour";
+				String user="root";
+				String password="achyut";
+				Connection con=null;
+				
+				
+					con=DriverManager.getConnection(url, user, password);
+					System.out.println("\n Connected successfully");
+					Statement stmt = con.createStatement();
+					
+				
+				
+				
+				
+		PrintWriter out=response.getWriter();
+		response.setContentType("text/html");// TODO Auto-generated method stub
+		String username=request.getParameter("usermail");
+		String password1=request.getParameter("userpass");
+		String fname=request.getParameter("firstname");
+		String lname=request.getParameter("lastname");
+		String no=request.getParameter("mno");
+		String address=request.getParameter("address");
+		String birth =request.getParameter("dob");
+		
+		/*SimpleDateFormat formatter=new SimpleDateFormat("yyyy/mm/dd");
+		try {
+			Date date=formatter.parse(birth);
+			 System.out.println(date);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		
+		
+       
+        System.out.println(no);
+				
+        String query = "INSERT INTO customer(firstname,lastname,emailid,phoneno,dob,address,password)" + 
+		           "VALUES ('" + fname + "', '" + lname + "', '" + username + "','"+no+"','"+birth+"','"+address+"','"+password1+"')";
+		
+			
+        int i = stmt.executeUpdate(query);
+        if (i != 0) {
+        System.out.println("Record has been inserted successfully in Customer table");
+        } else {
+        System.out.println("Inserting record get failure");
+        }	 
+
+		String query1="INSERT INTO login (username,password) " + 
+		           "VALUES ('" + username + "', '" + password1 + "')";
+		
+        int a = stmt.executeUpdate(query1);
+        if (a != 0) {
+        System.out.println("Record has been inserted successfully in Login Table");
+        } else {
+        System.out.println("Inserting record get failure");
+        }	 
+
+         		
+        
+        
+        
+		}catch(SQLException e)
+		{
+			System.out.println(e);
+		}
+		
+		catch(ClassNotFoundException e)
+		{
+			System.out.println("Driver not found");
+		}
+		
+
+		
 	}
 
 }
